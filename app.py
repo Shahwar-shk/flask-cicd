5d3145c815fbc3aebff8201935261ba9de5e94d8
Shahwar <80305334+Shahwar-shk@users.noreply.github.com>
2025-04-29 20:17:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-# app.py
 from flask import Flask, render_template
-from views import main_blueprint  # Import your blueprint if applicable
 
 app = Flask(__name__)
 
-# Register the blueprint (if you're using one)
-app.register_blueprint(main_blueprint)
-
 @app.route('/')
 def home():
-    return render_template('index.html')  # Render the 'index.html' file from the templates folder
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
